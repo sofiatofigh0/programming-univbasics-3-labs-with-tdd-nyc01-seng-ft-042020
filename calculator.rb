e3e6845db2a46cbd114fce872647c395219fd5c0
sofia tofigh <sst2140@columbia.edu>
2020-03-28 21:19:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 first_number = 5
 second_number = 3
 sum = first_number + second_number
-difference= second_number - first_number
+difference= first_number - second_number
 product= first_number * second_number
 quotient= (first_number) / (second_number)
 
